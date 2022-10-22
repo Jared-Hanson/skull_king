25fef83a23a9993bf5ff26b1eb39119d51db5217
Jared-Hanson <jrodhanson@gmail.com>
2022-10-22 16:49:09 -0600

    8u76gtiuygolig

--- machine_learning/fix_jpgs.py
+++ machine_learning/fix_jpgs.py
@@ -12,8 +12,31 @@ for im in os.scandir(f"number_model"):
     if ".DS_Store" not in im.path:
         print(im.path)
         img = cv2.imread(im.path)
-        text = pytesseract.image_to_string(cropped)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+        # Performing OTSU threshold
+        ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+        
+        # Specify structure shape and kernel size.
+        # Kernel size increases or decreases the area
+        # of the rectangle to be detected.
+        # A smaller value like (10, 10) will detect
+        # each word instead of a sentence.
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+        
+        # Applying dilation on the threshold image
+        dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+        
+        # Finding contours
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                                        cv2.CHAIN_APPROX_NONE)
+                                        
+        im2 = img.copy()
+            
+        # Open the file in append mode
+        file = open("recognized.txt", "a")
+        
+        # Apply OCR on the cropped image
+        text = pytesseract.image_to_string(im2[15:55, 15:55])
         print(text)
-        cv2.imshow("i", img)
-        cv2.waitKey(0)
 
